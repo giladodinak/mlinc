d7abfb2de8c1e3b9e2325bf81e187a9425b0b816
giladodinak <mlinc@odinak.com>
2026-08-21 11:53:57 -0700

    typo

--- src/float.h
+++ src/float.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2023-2024 Gilad Odinak */
-/* Basic operations for manipulation ifloating point numbers */
+/* Basic operations for manipulation floating point numbers */
 #ifndef FLOAT_H
 #define FLOAT_H
 
